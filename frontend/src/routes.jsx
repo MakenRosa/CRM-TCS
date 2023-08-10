e7c741ef32bc2be7eedf03f601dd5cdc965ea7b6
MakenRosa <maken.cristhian@gmail.com>
2023-08-10 13:09:52 -0300

    feat(frontend): add routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,14 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from 'pages/Login'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate
+} from 'react-router-dom'
 import { CssBaseline } from '@mui/material'
+import { Register } from 'pages/Login/Register'
+import { ForgotPassword } from 'pages/Login/ForgotPassword'
+import { Login } from 'pages/Login/Login'
 
 export default function AppRouter() {
 	return (
 		<Router>
 			<CssBaseline />
 			<Routes>
-				<Route path="/" element={<Login />} />
+				<Route path="/" element={<NotFound />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/forgot-password" element={<ForgotPassword />} />
 			</Routes>
 		</Router>
 	)
+}
+
+function NotFound() {
+	return <Navigate to="/login" replace />
 }
