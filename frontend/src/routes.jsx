7252ac740caba1f60fdc389e2ad1aba99f3db675
makenrosa <maken.cristhian@gmail.com>
2023-08-09 23:57:35 -0300

    refactor(frontend): add reset on routes.jsx

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
+import Login from 'pages/Login'
+import { CssBaseline } from '@mui/material'
 
 export default function AppRouter() {
 	return (
 		<Router>
+			<CssBaseline />
 			<Routes>
 				<Route path="/" element={<Login />} />
 			</Routes>
