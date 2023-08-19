ca0193c6af2e8efd4d7d03b13211f48c49013eee
martyin.okuma <martyin.okuma@nexxera.com>
2023-08-19 20:12:19 -0300

    refactor(backend): login screen endpoints

--- backend/usuario/serializers.py
+++ backend/usuario/serializers.py
@@ -1,8 +1,11 @@
-from rest_framework import  serializers
-from .models import Usuario
+from djoser.serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+
+
+Usario = get_user_model()
 
 #Faz o link entre o que está na base e transforma em JSON
-class UsuarioSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ['id', 'email', 'senha', 'nome', 'cpf']
+class UsuarioSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = Usario
+        fields = ('id', 'email', 'password', 'first_name', 'cpf')
