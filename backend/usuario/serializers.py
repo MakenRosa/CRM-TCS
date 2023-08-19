2272a3264b4bcff3b5e06f70de3546054851af5c
martyin.okuma <martyin.okuma@nexxera.com>
2023-08-19 20:15:58 -0300

    refactor(backend): correcao ortografica kkkk

--- backend/usuario/serializers.py
+++ backend/usuario/serializers.py
@@ -2,10 +2,10 @@ from djoser.serializers import UserCreateSerializer
 from django.contrib.auth import get_user_model
 
 
-Usario = get_user_model()
+Usuario = get_user_model()
 
 #Faz o link entre o que está na base e transforma em JSON
 class UsuarioSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
-        model = Usario
+        model = Usuario
         fields = ('id', 'email', 'password', 'first_name', 'cpf')
