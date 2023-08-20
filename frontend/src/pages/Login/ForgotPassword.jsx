8cac0c9a257ea595c9ac1e82838e6281a98a6908
makenrosa <maken.cristhian@gmail.com>
2023-08-20 19:12:32 -0300

    refactor(frontend): add cancel button on forgotPassword

--- frontend/src/pages/Login/ForgotPassword.jsx
+++ frontend/src/pages/Login/ForgotPassword.jsx
@@ -1,33 +1,42 @@
-import { AuthLayout } from "./AuthLayout"
-import Button from "components/Button"
+import Button from "components/Button/Button"
 import { Email } from "@mui/icons-material"
-import Form from "components/Form"
+import Form from "components/Form/Form"
+import { Link } from "react-router-dom"
 import { SectionLogin } from "./SectionLogin"
-import { TextField } from "components/TextField"
+import { StyledLinks } from "./Login.styles"
+import { TextField } from "components/TextField/TextField"
 
 export const ForgotPassword = () => 
-  <AuthLayout>
-    <SectionLogin title="Recuperar Senha">
-      <p>
-        Informe seu e-mail para receber instruções sobre como redefinir sua
-        senha.
-      </p>
+  <SectionLogin title="Recuperar Senha">
+    <p>
+      Informe seu e-mail para receber instruções sobre como redefinir sua
+      senha.
+    </p>
 
-      <Form>
-        <TextField
-          fullWidth
-          icon={<Email />}
-          placeholder="Email"
-          position="start"
-          size="small"
-          type="email"
-          variant="filled"
-        />
-
+    <Form>
+      <TextField
+        fullWidth
+        icon={<Email />}
+        placeholder="Email"
+        position="start"
+        size="small"
+        type="email"
+        variant="filled"
+      />
+      <StyledLinks maxHeight="40px">
+        <Button
+          className="btn--secondary"
+          component={Link}
+          to="/login"
+          variant="outlined"
+        >
+          Cancelar
+        </Button>
         <Button className="btn--primary" type="submit" variant="contained">
           Enviar
         </Button>
-      </Form>
-    </SectionLogin>
-  </AuthLayout>
+      </StyledLinks>
+    </Form>
+  </SectionLogin>
+
 
