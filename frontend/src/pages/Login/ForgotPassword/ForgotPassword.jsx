7728ad6c87f6e38c68b0ca4c2c541a9e355c6f5f
MakenRosa <maken.cristhian@gmail.com>
2023-08-21 19:07:34 -0300

    refactor(frontend): add redirection on forgotpassword

--- frontend/src/pages/Login/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/pages/Login/ForgotPassword/ForgotPassword.jsx
@@ -1,38 +1,49 @@
 import { Button, Form, TextField } from "components"
 import { Email } from "@mui/icons-material"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { StyledLinks, SectionLogin } from "pages"
+import { useEffect, useState } from "react"
 
-export const ForgotPassword = () => 
-  <SectionLogin h="50%" title="Recuperar Senha">
-    <p>
-      Informe seu e-mail para receber instruções sobre como redefinir sua
-      senha.
-    </p>
-    <Form>
-      <TextField
-        fullWidth
-        icon={<Email />}
-        placeholder="Email"
-        position="start"
-        size="small"
-        type="email"
-        variant="filled"
-      />
-      <StyledLinks maxHeight="40px">
-        <Button
-          className="btn--secondary"
-          component={Link}
-          to="/login"
-          variant="outlined"
-        >
-          Cancelar
-        </Button>
-        <Button className="btn--primary" type="submit" variant="contained">
-          Enviar
-        </Button>
-      </StyledLinks>
-    </Form>
-  </SectionLogin>
+export const ForgotPassword = () => {
+  const [isLogged] = useState(!!sessionStorage.getItem("token"))
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    if (isLogged) {
+      navigate('/dashboard')
+    }
+  }, [isLogged, navigate])
+  return (
+    <SectionLogin h="50%" title="Recuperar Senha">
+      <p>
+        Informe seu e-mail para receber instruções sobre como redefinir sua
+        senha.
+      </p>
+      <Form>
+        <TextField
+          fullWidth
+          icon={<Email />}
+          placeholder="Email"
+          position="start"
+          size="small"
+          type="email"
+          variant="filled"
+        />
+        <StyledLinks maxHeight="40px">
+          <Button
+            className="btn--secondary"
+            component={Link}
+            to="/login"
+            variant="outlined"
+          >
+            Cancelar
+          </Button>
+          <Button className="btn--primary" type="submit" variant="contained">
+            Enviar
+          </Button>
+        </StyledLinks>
+      </Form>
+    </SectionLogin>
+  )
+}
 
