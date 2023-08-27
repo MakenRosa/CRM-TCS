b696c9453984eabaaa801d5926a99cf815efd9fd
makenrosa <maken.cristhian@gmail.com>
2023-08-27 16:11:31 -0300

    refactor(frontend): fix error messages

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,15 @@
 import axios from "axios"
 import jwtDecode from "jwt-decode"
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 // Constantes para códigos de status HTTP
 const CONFLICT = 409
 const FORBIDDEN = 403
 const INTERNAL_SERVER_ERROR = 500
 const UNAUTHORIZED = 401
+
+const MS_PER_SECOND = 1000
 
 // URLs de endpoints para autenticação
 const LOGIN_URL = "/auth/login"
@@ -18,7 +20,7 @@ const refreshToken = async () => {
   const refresh_token = sessionStorage.getItem("refresh_token")
   try {
     const response = await api.post(REFRESH_TOKEN_URL, {}, {
-      headers: { 'Authorization': `Bearer ${ refresh_token }` }
+      headers: { Authorization: `Bearer ${ refresh_token }` }
     })
     sessionStorage.setItem("token", response.data.access_token)
     return response.data.access_token
@@ -27,86 +29,93 @@ const refreshToken = async () => {
   }
 }
 
-
-// Função para manipular respostas de erro e exibir mensagens relevantes
 const handleErrorResponse = async error => {
-  let message = error?.response?.data?.message || "Um erro ocorreu. Por favor, tente novamente."
-  
-  if (error?.response?.status === UNAUTHORIZED) {
-    try {
-      const newToken = await refreshToken()
-      error.config.headers['Authorization'] = `Bearer ${ newToken }`
-      return api.request(error.config)
-    } catch (refreshError) {
-      sessionStorage.removeItem("token")
-      sessionStorage.removeItem("refresh_token")
-      window.location.href = '/login' // redirecionar para a página de login
+  const status = error?.response?.status
+  const errorMessage = error?.response?.data?.message
+
+  if (status === UNAUTHORIZED) {
+    // Captura mensagem específica para erro de login
+    if (error.config.url.endsWith(LOGIN_URL)) {
+      toast.error("Usuário e/ou senha inválidos.")
+      return Promise.reject(error)
+    }
+
+    if (error.config.url !== REFRESH_TOKEN_URL) {
+      try {
+        const newToken = await refreshToken()
+        error.config.headers['Authorization'] = `Bearer ${ newToken }`
+        return api.request(error.config)
+      } catch (refreshError) {
+        logoutUser()
+        return Promise.reject(error)
+      }
+    } else {
+      logoutUser()
+      return Promise.reject(error)
     }
   }
-  switch (error?.response?.status) {
-    case CONFLICT:
-      message = error?.response?.data?.message || "Este E-mail já está cadastrado."
-      break
-    case FORBIDDEN:
-      message = error?.response?.data?.message || "Você não tem permissão para acessar este recurso."
-      break
-    case INTERNAL_SERVER_ERROR:
-      message = error?.response?.data?.message || "Erro interno do servidor. Tente novamente mais tarde."
-      break
-    default:
-      break
-  }
-  
+
+  const message = getErrorMessage(status, errorMessage)
   toast.error(message)
   return Promise.reject(error)
 }
 
-// Criação da instância do Axios com URL base
+const getErrorMessage = (status, errorMessage) => {
+  const defaultMessages = {
+    [CONFLICT]: "Este E-mail já está cadastrado.",
+    [FORBIDDEN]: "Você não tem permissão para acessar este recurso.",
+    [INTERNAL_SERVER_ERROR]: "Erro interno do servidor. Tente novamente mais tarde."
+  }
+
+  return errorMessage || defaultMessages[status] || "Um erro ocorreu. Por favor, tente novamente."
+}
+
+const logoutUser = () => {
+  sessionStorage.removeItem("token")
+  sessionStorage.removeItem("refresh_token")
+  window.location.href = "/login"
+}
+
 const api = axios.create({
   baseURL: "http://localhost:8000"
 })
 
-// Interceptor para adicionar o token ao cabeçalho, se existir
 api.interceptors.request.use(
   config => {
     const token = getToken()
     if (token) {
-      config.headers['Authorization'] = `Bearer ${ token }`
+      config.headers["Authorization"] = `Bearer ${ token }`
     }
     return config
   },
   error => Promise.reject(error)
 )
 
-// Interceptor para lidar com a resposta, incluindo sucesso e erro
 api.interceptors.response.use(
   response => {
     if (response?.data?.message) {
-      toast.success(response.data.message) // Mostra a mensagem de sucesso, se existir
+      toast.success(response.data.message)
     }
     return response
   },
-  handleErrorResponse // Atualizado para usar a versão nova que inclui a lógica de refreshToken
+  handleErrorResponse
 )
 
-// Funções para interagir com os endpoints de autenticação
-const loginUser = user => api.post(LOGIN_URL, user)
-const registerUser = user => api.post(REGISTER_URL, user)
-
-export { api, loginUser, registerUser, refreshToken }
-
-const getToken=() => {
+const getToken = () => {
   const token = sessionStorage.getItem("token")
   if (!token) {
     return null
-  } 
+  }
   const decoded = jwtDecode(token)
-  const currentTime = Date.now() / 1000
+  const currentTime = Date.now() / MS_PER_SECOND
   const isValid = decoded.exp > currentTime
-  console.log(isValid)
-  if(isValid) {
+  if (isValid) {
     return token
-  } 
-    return null
-  
+  }
+  return null
 }
+
+const loginUser = user => api.post(LOGIN_URL, user)
+const registerUser = user => api.post(REGISTER_URL, user)
+
+export { api, loginUser, registerUser, refreshToken, logoutUser }
--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,3 +1,2 @@
 export * from './api'
 export * from './validateForm'
-export * from './refreshToken'
