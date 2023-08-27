fc1457bb8fa4c0e6d74aabe9d451cf55b2e3b6ba
makenrosa <maken.cristhian@gmail.com>
2023-08-27 19:36:08 -0300

    refactor(frontend): externalize authenticator funcionality

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,26 +1,10 @@
-import { Button } from 'components'
-import { useEffect } from 'react'
-import { useNavigate } from "react-router-dom"
+import { Button, WithAuth } from 'components'
 import { logoutUser } from 'utils'
 
-export const Dashboard = () => {
-    const navigate = useNavigate() // Função para navegar entre rotas
-    const isAuth = !!sessionStorage.getItem("token") // Verifica se o usuário está autenticado com base no token na sessionStorage
+const DashboardComponent = () => 
+  <div>
+    <h1>Dashboard</h1> 
+    <Button onClick={logoutUser}>Logout</Button> 
+  </div>  // Renderiza o conteúdo do painel se o usuário estiver autenticado
     
-    useEffect(() => {
-        if (!isAuth) {
-            navigate('/login') // Se não estiver autenticado, redireciona para a página de login
-        }
-    }, [navigate, isAuth])
-
-    if (!isAuth) {
-        return null // Não renderiza nada se o usuário não estiver autenticado
-    }
-    
-    return(
-      <div>
-        <h1>Dashboard</h1> 
-        <Button onClick={logoutUser}>Logout</Button> 
-      </div>  // Renderiza o conteúdo do painel se o usuário estiver autenticado
-    )
-}
+export const Dashboard = WithAuth(DashboardComponent) // Aplica o HOC WithAuth ao componente DashboardComponent
