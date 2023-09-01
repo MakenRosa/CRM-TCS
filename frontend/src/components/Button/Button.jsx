565266a2f3ede17f612c803297f7fe4ea87cf0e7
MakenRosa <maken.cristhian@gmail.com>
2023-09-01 14:37:16 -0300

    refactor(frontend): update components behavior

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,18 +1,16 @@
-import { Button as BtnMui, styled } from '@mui/material'
 import PropTypes from 'prop-types'
+import { StyledButton } from '.'
 
-// StyledButton é um botão customizado usando styled-components
-const StyledButton = styled(BtnMui)`
-`
 
 // Componente Button que aceita children, disabled e outros props
-export const Button = ({ children, disabled, ...props }) => 
-  <StyledButton disabled={disabled} {...props}>
+export const Button = ({ children, disabled, variant, ...props }) => 
+  <StyledButton disabled={disabled} variant={variant} {...props}>
     {children} 
   </StyledButton>
 
 // Definindo os tipos de propriedades esperados
 Button.propTypes = {
   children: PropTypes.node.isRequired, // Conteúdo dentro do botão (como texto ou ícones)
-  disabled: PropTypes.bool // Indica se o botão está desabilitado
+  disabled: PropTypes.bool, // Indica se o botão está desabilitado
+  variant: PropTypes.string // Indica a variante do botão (contained, outlined, text)
 }
---
+++ frontend/src/components/PasswordValidator/PasswordValidator.styles.js
@@ -0,0 +1,22 @@
+const { styled, Box } = require("@mui/material")
+
+export const StyledParagraph = styled('p')({
+    display: 'flex',
+    alignItems: 'center',
+    gap: '4px',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    color: '#666666',
+    margin: '0'
+})
+
+export const StyledBox = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    gap: '4px',
+    padding: '8px',
+    border: '1px solid #666666',
+    borderRadius: '5px',
+    backgroundColor: '#f2f2f2'
+})
