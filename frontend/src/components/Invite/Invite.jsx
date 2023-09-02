713c7c5c4e0e91703011148f7e8a8ebef2968604
makenrosa <maken.cristhian@gmail.com>
2023-09-02 14:25:33 -0300

    refactor(frontend): remove button out of modal

--- frontend/src/components/Invite/Invite.jsx
+++ frontend/src/components/Invite/Invite.jsx
@@ -11,26 +11,21 @@ export const Invite = () => {
   }
 
   return (
-    <>
-      <StyledModal
-        aria-describedby="modal-modal-description"
-        aria-labelledby="modal-modal-title"
-        onClose={() => setOpen(false)}
-        open={open}
-      >
-        <StyledModalBox>
-          <h1>SOLVE CRM</h1>
-          <p>Insira o e-mail para convidar:</p>
-          <TextField fullWidth={false} icon={<Email />} placeholder="E-mail" type="email" />
-          <Button onClick={handleSendInvite}
-            variant="primary" 
-          >Enviar convite
-          </Button>
-        </StyledModalBox>
-      </StyledModal>
-      <Button onClick={() => setOpen(true)}>
-        Abrir modal
-      </Button>
-    </>
+    <StyledModal
+      aria-describedby="modal-modal-description"
+      aria-labelledby="modal-modal-title"
+      onClose={() => setOpen(false)}
+      open={open}
+    >
+      <StyledModalBox>
+        <h1>SOLVE CRM</h1>
+        <p>Insira o e-mail para convidar:</p>
+        <TextField fullWidth={false} icon={<Email />} placeholder="E-mail" type="email" />
+        <Button onClick={handleSendInvite}
+          variant="primary"
+        >Enviar convite
+        </Button>
+      </StyledModalBox>
+    </StyledModal>
   )
 }
