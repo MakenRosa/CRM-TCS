7850d42cd6d04d4b58eb49a8f0f24237db655e2b
makenrosa <maken.cristhian@gmail.com>
2023-09-04 16:17:35 -0300

    refactor(frontend): add filter on dashboard

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,49 @@
+import { MenuItem } from '@mui/material'
 import { Button, WithAuth } from 'components'
-import { logoutUser } from 'utils'
 
-const DashboardComponent = () => 
-  <div>
-    <h1>Dashboard</h1> 
-    <Button onClick={logoutUser}>Logout</Button> 
-  </div>  
-    
+import { useState } from 'react'
+import { StyledCardBox, StyledDashboard, StyledFilter, StyledFilterBox, StyledLabel, StyledSelect } from '.'
+
+const DashboardComponent = () => {
+  const [team, setTeam] = useState('Equipe Rocket')
+  const [funnel, setFunnel] = useState('Todos')
+  const [period, setPeriod] = useState('Diário')
+
+  return (
+    <StyledDashboard>
+      <StyledFilterBox>
+        <StyledFilter>
+          <StyledLabel variant="p">
+            Usuário/Equipe
+          </StyledLabel>
+          <StyledSelect onChange={e => setTeam(e.target.value)} value={team}>
+            <MenuItem value="Equipe Rocket">Equipe Rocket</MenuItem>
+          </StyledSelect>
+        </StyledFilter>
+        <StyledFilter>
+          <StyledLabel variant="p">
+            Funil
+          </StyledLabel>
+          <StyledSelect onChange={e => setFunnel(e.target.value)} value={funnel}>
+            <MenuItem value="Todos">Todos</MenuItem>
+          </StyledSelect>
+        </StyledFilter>
+        <StyledFilter>
+          <StyledLabel variant="p">
+            Período
+          </StyledLabel>
+          <StyledSelect onChange={e => setPeriod(e.target.value)} value={period}>
+            <MenuItem value="Diário">Diário</MenuItem>
+          </StyledSelect>
+        </StyledFilter>
+        <Button variant="primary">
+          Limpar filtros
+        </Button>
+      </StyledFilterBox>
+      <StyledCardBox>
+        <h1>Dashboard</h1>
+      </StyledCardBox>
+    </StyledDashboard>
+  )
+}
 export const Dashboard = WithAuth(DashboardComponent) 
