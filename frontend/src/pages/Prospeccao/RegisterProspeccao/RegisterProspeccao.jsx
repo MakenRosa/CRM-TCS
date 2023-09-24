d601cfba39ea0f50d2fe519595b2708582f5680f
makenrosa <maken.cristhian@gmail.com>
2023-09-23 23:35:50 -0300

    feat(frontend): integrate prospeccao with backend

--- frontend/src/pages/Prospeccao/RegisterProspeccao/RegisterProspeccao.jsx
+++ frontend/src/pages/Prospeccao/RegisterProspeccao/RegisterProspeccao.jsx
@@ -10,9 +10,11 @@ import {
   StyledRegisterTextField,
   StyledRegisterTitle
 } from "components"
-import { Box, CircularProgress, Typography, styled } from "@mui/material"
-import { useState } from "react"
+import { Box, CircularProgress, MenuItem, Typography, styled } from "@mui/material"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { createProspeccao, getLeads, updateProspeccao, validateProspection } from "utils"
+import { toast } from "react-toastify"
 
 const StyledRegisterProspeccaoSection = styled(Box)`
   display: flex;
@@ -28,33 +30,87 @@ const StyledSectionTitle = styled(Typography)`
   line-height: 1.2;
 `
 
-export const RegisterProspeccao = () => {
+const formatDate = date => {
+  if (date) {
+    return new Date(date).toISOString()
+  }
+  return ''
+}
+
+export const RegisterProspeccao = prospeccao => {
   const navigate = useNavigate()
-  // Campos da Prospecção
-  const [nomeNegocio, setNomeNegocio] = useState('')
-  const [lead, setLead] = useState('')
-  const [segmento, setSegmento] = useState('')
-  const [servicosProdutos, setServicosProdutos] = useState('')
-  const [participacaoComercial, setParticipacaoComercial] = useState('')
-  const [participacaoEfetiva, setParticipacaoEfetiva] = useState('')
-  const [consultor, setConsultor] = useState('')
-  const [dataInicioProspeccao, setDataInicioProspeccao] = useState(new Date().toISOString().slice())
-  const [dataContatoInicial, setDataContatoInicial] = useState(new Date().toISOString().slice())
-  const [dataProximaAcao, setDataProximaAcao] = useState(new Date().toISOString().slice())
-  const [preferenciaContato, setPreferenciaContato] = useState('')
-  const [horarioContato, setHorarioContato] = useState('')
-  const [observacoesAdicionais, setObservacoesAdicionais] = useState('')
+  const [leads, setLeads] = useState([])
+  
+  const [nomeNegocio, setNomeNegocio] = useState(prospeccao?.nome_negocio || '')
+  const [lead, setLead] = useState(prospeccao?.lead || '')
+  const [segmento, setSegmento] = useState(prospeccao?.segmento || '')
+  const [servicosProdutos, setServicosProdutos] = useState(prospeccao?.servicos_produtos || '')
+  const [participacaoComercial, setParticipacaoComercial] = useState(prospeccao?.participacao_comercial || '')
+  const [participacaoEfetiva, setParticipacaoEfetiva] = useState(prospeccao?.participacao_efetiva || '')
+  const [consultor, setConsultor] = useState(prospeccao?.consultor || '')
+  const [dataInicioProspeccao, setDataInicioProspeccao] = useState(formatDate(prospeccao?.data_inicio_prospeccao) || new Date().toISOString())
+  const [dataContatoInicial, setDataContatoInicial] = useState(formatDate(prospeccao?.data_contato_incial) || new Date().toISOString())
+  const [dataProximaAcao, setDataProximaAcao] = useState(formatDate(prospeccao?.data_proxima_acao) || new Date().toISOString())
+  const [preferenciaContato, setPreferenciaContato] = useState(prospeccao?.preferencia_contato || '')
+  const [horarioContato, setHorarioContato] = useState(prospeccao?.horario_contato || '')
+  const [observacoesAdicionais, setObservacoesAdicionais] = useState(prospeccao?.observacao || '')
   const [loading, setLoading] = useState(false)
 
-  // Handlers para campos
-  // ... (handlers para cada campo)
+
+  const handleNomeNegocio = event => setNomeNegocio(event.target.value)
+  const handleLead = event => setLead(event.target.value)
+  const handleSegmento = event => setSegmento(event.target.value)
+  const handleServicosProdutos = event => setServicosProdutos(event.target.value)
+  const handleParticipacaoComercial = event => setParticipacaoComercial(event.target.value)
+  const handleParticipacaoEfetiva = event => setParticipacaoEfetiva(event.target.value)
+  const handleConsultor = event => setConsultor(event.target.value)
+  const handleDataInicioProspeccao = event => setDataInicioProspeccao(event.target.value)
+  const handleDataContatoInicial = event => setDataContatoInicial(event.target.value)
+  const handleDataProximaAcao = event => setDataProximaAcao(event.target.value)
+  const handlePreferenciaContato = event => setPreferenciaContato(event.target.value)
+  const handleHorarioContato = event => setHorarioContato(event.target.value)
+  const handleObservacoesAdicionais = event => setObservacoesAdicionais(event.target.value)
 
   const handleSubmit = async event => {
     event.preventDefault()
     setLoading(true)
-
-    navigate('/oportunidades')
+    const data = {
+      nome_negocio: nomeNegocio,
+      lead,
+      segmento,
+      servicos_produtos: servicosProdutos,
+      participacao_comercial: participacaoComercial,
+      participacao_efetiva: participacaoEfetiva,
+      consultor,
+      // converter para dd-mm-yyyy (replace '/' por '-')
+      data_inicio_prospeccao: new Date(dataInicioProspeccao).toLocaleDateString('pt-BR').replace(/\//g, '-'),
+      data_contato_incial: new Date(dataContatoInicial).toLocaleDateString('pt-BR').replace(/\//g, '-'),
+      data_proxima_acao: new Date(dataProximaAcao).toLocaleDateString('pt-BR').replace(/\//g, '-'),
+      preferencia_contato: preferenciaContato,
+      horario_contato: horarioContato,
+      observacao: observacoesAdicionais,
+      status: 'Em prospecção'
+    }
+    try {
+      if (validateProspection(data)) {
+        prospeccao.id ? await updateProspeccao(prospeccao.id, data) : await createProspeccao(data)
+        navigate('/prospeccoes')
+      }
+    } catch (error) {
+      toast.error('Erro ao salvar prospecção!')
+    } finally {
+      setLoading(false)
+    }
+    
   }
+
+  useEffect(() => {
+    const fetchLeads = async () => {
+      const response = await getLeads()
+      setLeads(response.data.data.leads)
+    }
+    fetchLeads()
+  }, [])
 
   return (
     <StyledRegisterContainer>
@@ -64,43 +120,47 @@ export const RegisterProspeccao = () => {
           <StyledRegisterSection>
             <StyledRegisterProspeccaoSection>
               <StyledSectionTitle align="center" variant="h6">Informações Negócio</StyledSectionTitle>
-              <StyledRegisterTextField label="Nome Negócio" onChange={e => setNomeNegocio(e.target.value)} size="small" value={nomeNegocio} />
-              <StyledRegisterTextField label="Lead" onChange={e => setLead(e.target.value)} select size="small" value={lead} >
-                {/* Opções de Leads aqui */}
+              <StyledRegisterTextField label="Nome Negócio" onChange={handleNomeNegocio} size="small" value={nomeNegocio} />
+              <StyledRegisterTextField label="Lead" onChange={handleLead} select size="small" value={lead} >
+                {leads.map(itemLead => (
+                  <MenuItem key={itemLead.cnpj} value={itemLead.id}>
+                    {itemLead.nomeEmpresa}
+                  </MenuItem>
+                ))}
               </StyledRegisterTextField>
-              <StyledRegisterTextField label="Segmento" onChange={e => setSegmento(e.target.value)} size="small" value={segmento} />
+              <StyledRegisterTextField label="Segmento" onChange={handleSegmento} size="small" value={segmento} />
             </StyledRegisterProspeccaoSection>
             
             <StyledRegisterProspeccaoSection>
               <StyledSectionTitle align="center" variant="h6">Possibilidades Comerciais</StyledSectionTitle>
-              <StyledRegisterTextField label="Serviços/Produtos" onChange={e => setServicosProdutos(e.target.value)} size="small" value={servicosProdutos} />
+              <StyledRegisterTextField label="Serviços/Produtos" onChange={handleServicosProdutos} size="small" value={servicosProdutos} />
             </StyledRegisterProspeccaoSection>
 
             <StyledRegisterProspeccaoSection>
               <StyledSectionTitle align="center" variant="h6">Dados Participação Comercial</StyledSectionTitle>
-              <StyledRegisterTextField label="Participação Comercial" onChange={e => setParticipacaoComercial(e.target.value)} size="small" value={participacaoComercial} />
-              <StyledRegisterTextField label="Participação Efetiva" onChange={e => setParticipacaoEfetiva(e.target.value)} size="small" value={participacaoEfetiva} />
-              <StyledRegisterTextField label="Consultor" onChange={e => setConsultor(e.target.value)} size="small" value={consultor} />
+              <StyledRegisterTextField label="Participação Comercial" onChange={handleParticipacaoComercial} size="small" value={participacaoComercial} />
+              <StyledRegisterTextField label="Participação Efetiva" onChange={handleParticipacaoEfetiva} size="small" value={participacaoEfetiva} />
+              <StyledRegisterTextField label="Consultor" onChange={handleConsultor} size="small" value={consultor} />
             </StyledRegisterProspeccaoSection>
           </StyledRegisterSection>
           
           <StyledRegisterSection>
             <StyledRegisterProspeccaoSection>
               <StyledSectionTitle align="center" variant="h6">Dados relevantes</StyledSectionTitle>
-              <StyledRegisterTextField label="Data Início Prospecção" onChange={e => setDataInicioProspeccao(e.target.value)} size="small" type="date" value={dataInicioProspeccao} />
-              <StyledRegisterTextField label="Data Contato Inicial" onChange={e => setDataContatoInicial(e.target.value)} size="small" type="date" value={dataContatoInicial} />
-              <StyledRegisterTextField label="Data Próxima Ação" onChange={e => setDataProximaAcao(e.target.value)} size="small" type="date" value={dataProximaAcao} />
+              <StyledRegisterTextField label="Data Início Prospecção" onChange={handleDataInicioProspeccao} size="small" type="date" value={dataInicioProspeccao} />
+              <StyledRegisterTextField label="Data Contato Inicial" onChange={handleDataContatoInicial} size="small" type="date" value={dataContatoInicial} />
+              <StyledRegisterTextField label="Data Próxima Ação" onChange={handleDataProximaAcao} size="small" type="date" value={dataProximaAcao} />
             </StyledRegisterProspeccaoSection>
             
             <StyledRegisterProspeccaoSection>
               <StyledSectionTitle align="center" variant="h6">Canais de Comunicação</StyledSectionTitle>
-              <StyledRegisterTextField label="Preferência de Contato" onChange={e => setPreferenciaContato(e.target.value)} size="small" value={preferenciaContato} />
-              <StyledRegisterTextField label="Horário para Contato" onChange={e => setHorarioContato(e.target.value)} size="small" value={horarioContato} />
+              <StyledRegisterTextField label="Preferência de Contato" onChange={handlePreferenciaContato} size="small" value={preferenciaContato} />
+              <StyledRegisterTextField label="Horário para Contato" onChange={handleHorarioContato} size="small" value={horarioContato} />
             </StyledRegisterProspeccaoSection>
             
             <StyledRegisterProspeccaoSection>
               <StyledSectionTitle align="center" variant="h6">Observações/Notas</StyledSectionTitle>
-              <StyledRegisterTextField label="Observações adicionais" multiline onChange={e => setObservacoesAdicionais(e.target.value)} rows={5} size="small" value={observacoesAdicionais} />
+              <StyledRegisterTextField label="Observações adicionais" multiline onChange={handleObservacoesAdicionais} rows={5} size="small" value={observacoesAdicionais} />
             </StyledRegisterProspeccaoSection>
           </StyledRegisterSection>
         </StyledRegisterBox>
