6b48d53d703a3ed3486c11af342d096fda60df99
makenrosa <maken.cristhian@gmail.com>
2023-10-12 20:22:56 -0300

    feat(frontend): add integration with backend

--- frontend/src/pages/Teams/Team/Team.jsx
+++ frontend/src/pages/Teams/Team/Team.jsx
@@ -1,40 +1,68 @@
 import { ExpandLess, ExpandMore, GroupsOutlined, RadioButtonUnchecked } from "@mui/icons-material"
 import { Box, Checkbox, Typography } from "@mui/material"
-import { Button } from "components"
+import { Button, Invite } from "components"
 import { useState } from "react"
 import PropTypes from 'prop-types'
+import { deleteUser } from "utils"
 import { StyledCheckedIcon, StyledTeam } from "./Team.styles"
 
-export const Team = ({ team }) => {
+export const Team = ({ team, title }) => {
   const [isCollapsed, setIsCollapsed] = useState(false)
+  const [isInviteOpen, setIsInviteOpen] = useState(false)
+  const [isUserStaff, setIsUserStaff] = useState(false)
+  
+  const [selectedIds, setSelectedIds] = useState([])
+
+  const openInviteModal = () => {
+    setIsInviteOpen(true)
+  }
+
+  const toggleId = (id, isChecked) => {
+    if (isChecked) {
+      setSelectedIds([...selectedIds, id])
+    } else {
+      setSelectedIds(selectedIds.filter(selectedId => selectedId !== id))
+    }
+  }
+  
+  const handleDelete = () => {
+    selectedIds.forEach(id => {
+      deleteUser(id)
+        .then(() => {
+          setSelectedIds(selectedIds.filter(selectedId => selectedId !== id))
+        })
+    }
+  )}
 
   return (
-    <StyledTeam isCollapsed={isCollapsed}>
-      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 3 }}>
-          <GroupsOutlined sx={{ fontSize: 40, marginRight: '1rem', display: 'flex', alignItems: 'center' }} />
-          <Typography
-            fontSize={30} 
-            fontWeight="bold" 
-            sx={{ color: '#9181f4' }}
-          >
-            {team.titulo}
-          </Typography>
+    <>
+      <Invite onClose={() => setIsInviteOpen(false)} open={isInviteOpen} />
+      <StyledTeam isCollapsed={isCollapsed}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 3 }}>
+            <GroupsOutlined sx={{ fontSize: 40, marginRight: '1rem', display: 'flex', alignItems: 'center' }} />
+            <Typography
+              fontSize={30} 
+              fontWeight="bold" 
+              sx={{ color: '#9181f4' }}
+            >
+              {title}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+            <Checkbox
+              checkedIcon={<StyledCheckedIcon />} 
+              icon={<RadioButtonUnchecked />}
+              size="medium"
+              sx={{ display: isCollapsed ? 'none' : 'block' }}
+            />
+          </Box>
         </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-          <Checkbox
-            checkedIcon={<StyledCheckedIcon />} 
-            icon={<RadioButtonUnchecked />}
-            size="medium"
-            sx={{ display: isCollapsed ? 'none' : 'block' }}
-          />
-        </Box>
-      </Box>
-      {
+        {
         !isCollapsed && (
         <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', maxWidth: '1180px', marginTop: '1rem', marginLeft: '60px' }}>
           <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: '100px', gap: '1rem' }}>
-            {team.integrantes.map((integrante, index) => (
+            {team.map((integrante, index) => (
               <Box 
                 key={index} 
                 sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: '10rem' }}
@@ -43,45 +71,49 @@ export const Team = ({ team }) => {
                   <Checkbox 
                     checkedIcon={<StyledCheckedIcon />}
                     icon={<RadioButtonUnchecked />}
+                    onChange={e => toggleId(integrante.id, e.target.checked)}
                     size="small"
                   />
                   <Typography
                     fontSize={20}
                     variant="h6"
                   >
-                    {integrante.nome}
+                    {integrante.first_name}
                   </Typography>
                 </Box>
                 <Typography 
                   fontSize={20} 
                   variant="h6"
-                >{integrante.email}
+                >
+                  {integrante.email}
                 </Typography>
               </Box>
           ))}
           </Box>
           <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: '100px', gap: '1rem' }}>
-            <Button variant="primary">Editar</Button>
+            <Button onClick={openInviteModal} variant="primary">Convidar</Button>
             <Button variant="secondary">Excluir</Button>
           </Box>
         </Box>
       )}
-      <Box sx={{ display: 'flex', justifyContent: 'right', alignItems: 'right', position: `${ isCollapsed ? 'absolute' : 'relative' }`, right: `${ isCollapsed ? '20px' : '0px' }`, top: `${ isCollapsed ? '15px' : '0px' }` }} >
-        {isCollapsed ? 
-          <ExpandMore fontSize="large" onClick={() => setIsCollapsed(!isCollapsed)} sx={{ fontSize: 50, cursor: 'pointer' }} /> :
-          <ExpandLess fontSize="large" onClick={() => setIsCollapsed(!isCollapsed)} sx={{ fontSize: 50, cursor: 'pointer' }} />
+        <Box sx={{ display: 'flex', justifyContent: 'right', alignItems: 'right', position: `${ isCollapsed ? 'absolute' : 'relative' }`, right: `${ isCollapsed ? '20px' : '0px' }`, top: `${ isCollapsed ? '15px' : '0px' }` }} >
+          {isCollapsed ? 
+            <ExpandMore fontSize="large" onClick={() => setIsCollapsed(!isCollapsed)} sx={{ fontSize: 50, cursor: 'pointer' }} /> :
+            <ExpandLess fontSize="large" onClick={() => setIsCollapsed(!isCollapsed)} sx={{ fontSize: 50, cursor: 'pointer' }} />
         }
-      </Box>
-    </StyledTeam>
+        </Box>
+      </StyledTeam>
+    </>
   )
 }
 
 Team.propTypes = {
-  team: PropTypes.shape({
-    titulo: PropTypes.string,
-    integrantes: PropTypes.arrayOf(PropTypes.shape({
-      nome: PropTypes.string,
-      email: PropTypes.string
-    }))
-  })
+  team: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      email: PropTypes.string.isRequired,
+      first_name: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  title: PropTypes.string
 }
--- frontend/src/pages/Teams/Teams.jsx
+++ frontend/src/pages/Teams/Teams.jsx
@@ -1,24 +1,36 @@
 import { Box } from "@mui/material"
-import { Button, StyledRegisterBox, StyledRegisterContainer, StyledRegisterForm, StyledRegisterTitle } from "components"
+import { StyledRegisterBox, StyledRegisterContainer, StyledRegisterForm, StyledRegisterTitle } from "components"
+import { useEffect, useState } from "react"
+import { getTeam } from "utils"
 import { Team } from "./Team/Team"
-import { mockTimes } from "./data"
 import { StyledTeams } from "./Teams.styles"
 
-export const Teams = () => (
-  <StyledRegisterContainer>
-    <StyledRegisterTitle>Equipes Cadastradas</StyledRegisterTitle>
-    <StyledRegisterForm>
-      <StyledRegisterBox sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
-        <StyledTeams>
-          {mockTimes.map((team, index) => (
-            <Team key={index} team={team} />
-          ))}
-        </StyledTeams>
-      </StyledRegisterBox>
-      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: '1rem' }}>
-        <Button variant="secondary">Cancelar</Button>
-        <Button variant="primary">Excluir</Button>
-      </Box>
-    </StyledRegisterForm>
-  </StyledRegisterContainer>
+export const Teams = () => {
+  const [teams, setTeams] = useState([])
+  const [groupName, setGroupName] = useState()
+
+  const user_id = sessionStorage.getItem('user_id')
+  
+  useEffect(() => {
+    getTeam(user_id)
+      .then(response => {
+        setTeams(response.data.data.groups)
+        setGroupName(response.data.data.nome_grupo)
+      })
+  }
+  , [])
+
+  return (
+    <StyledRegisterContainer>
+      <StyledRegisterTitle>Equipes Cadastradas</StyledRegisterTitle>
+      <StyledRegisterForm>
+        <StyledRegisterBox sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+          <StyledTeams>
+            <Team team={teams} title={groupName} />
+          </StyledTeams>
+        </StyledRegisterBox>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', gap: '1rem' }} />
+      </StyledRegisterForm>
+    </StyledRegisterContainer>
   )
+          }
