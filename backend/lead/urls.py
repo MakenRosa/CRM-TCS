b24f4938b3eafad79082c20aae67584ba7587271
martyin.okuma <martyin.okuma@nexxera.com>
2023-10-23 16:15:53 -0300

    feat(backend): get lead by id endpoint

--- backend/lead/urls.py
+++ backend/lead/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from lead.views import Leads, LeadsDetails
+from lead.views import Leads, LeadsDetails, LeadsID
 
 urlpatterns = [
     path('', Leads.as_view()),
-    path('<str:cnpj>', LeadsDetails.as_view())
+    path('<str:cnpj>', LeadsDetails.as_view()),
+    path('<int:id>/', LeadsID.as_view())
 ]
