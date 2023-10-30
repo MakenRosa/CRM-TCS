827e5fe94058bc2238b32a2a9f1f456a18580c1f
MakenRosa <maken.cristhian@gmail.com>
2023-10-30 15:18:37 -0300

    feat(frontend): add download of charts

--- frontend/src/pages/Relatorios/SimpleFilter/SimpleFilter.jsx
+++ frontend/src/pages/Relatorios/SimpleFilter/SimpleFilter.jsx
@@ -7,36 +7,58 @@ import percentage from 'assets/percentage.svg'
 import finish from 'assets/finish.svg'
 import lost from 'assets/lost.svg'
 import { StyledFilterBox } from "pages/Relatorios/Relatorios.styles"
+import { getFunil, getMenuBI, getPropostaProspeccao, getProspeccaoLeads, getVendasProposta, getVendasProspeccao } from "utils"
+import { toast } from "react-toastify"
 
-export const SimpleFilter = () => (
-  <Box display="flex" flexDirection="column" gap="5px" height="80%" justifyContent="center" marginTop="50px" width="80%">
-    <Box alignItems="center" display="flex" flexDirection="row" gap="5px" justifyContent="space-between">
-      <StyledFilterBox>
-        <img alt="Currency" src={currency} style={{ height: "50%" }} />
-        <Typography color="#fff" variant="h6">Total de Vendas</Typography>
-      </StyledFilterBox>
-      <StyledFilterBox>
-        <img alt="Group" src={group} style={{ height: "50%" }} />
-        <Typography color="#fff" variant="h6">Equipes</Typography>
-      </StyledFilterBox>
-      <StyledFilterBox>
-        <img alt="Calendar" src={calendar} style={{ height: "50%" }} />
-        <Typography color="#fff" variant="h6">Agenda</Typography>
-      </StyledFilterBox>
-    </Box>
-    <Box alignItems="center" display="flex" flexDirection="row" gap="5px" justifyContent="space-between">
-      <StyledFilterBox>
-        <img alt="Percentage" src={percentage} style={{ height: "50%" }} />
-        <Typography color="#fff" variant="h6">Conversão</Typography>
-      </StyledFilterBox>
-      <StyledFilterBox>
-        <img alt="Finish" src={finish} style={{ height: "50%" }} />
-        <Typography color="#fff" variant="h6">Concluídos</Typography>
-      </StyledFilterBox>
-      <StyledFilterBox>
-        <img alt="Lost" src={lost} style={{ height: "50%" }} />
-        <Typography color="#fff" variant="h6">Perdidos</Typography>
-      </StyledFilterBox>
-    </Box>
-  </Box>
+const API_FUNCTIONS = {
+  "Propostas P/ Prospecção": getPropostaProspeccao,
+  "Prospecções P/ Lead": getProspeccaoLeads,
+  "Vendas -> propostas": getVendasProposta,
+  "Prospecções -> vendas": getVendasProspeccao,
+  "Funil de Vendas": getFunil,
+  "Menu BI": getMenuBI
+}
+
+export const SimpleFilter = () => { 
+  const user_id = sessionStorage.getItem("user_id")
+
+  const handleBoxClick = async title => {
+    if (API_FUNCTIONS[title]) {
+      const url = API_FUNCTIONS[title](user_id)
+      
+      const response = await fetch(url)
+  
+      if (response.ok) {
+        window.open(url, '_blank')
+        toast.success(`O download de ${ title } foi iniciado`)
+      } else {
+        toast.error(`Erro ao buscar dados de ${ title }`)
+      }
+    } else {
+      toast.error(`Erro ao buscar dados de ${ title }`)
+    }
+  }
+  
+
+  const renderBox = (alt, src, title) => (
+    <StyledFilterBox onClick={() => handleBoxClick(title)}>
+      <img alt={alt} src={src} style={{ height: "50%" }} />
+      <Typography color="#fff" variant="h6">{title}</Typography>
+    </StyledFilterBox>
   )
+
+  return (
+    <Box display="flex" flexDirection="column" gap="5px" height="80%" justifyContent="center" marginTop="50px" width="80%">
+      <Box alignItems="center" display="flex" flexDirection="row" gap="5px" justifyContent="space-between">
+        {renderBox("Currency", currency, "Propostas P/ Prospecção")}
+        {renderBox("Group", group, "Prospecções P/ Lead")}
+        {renderBox("Calendar", calendar, "Vendas -> propostas")}
+      </Box>
+      <Box alignItems="center" display="flex" flexDirection="row" gap="5px" justifyContent="space-between">
+        {renderBox("Percentage", percentage, "Prospecções -> vendas")}
+        {renderBox("Finish", finish, "Funil de Vendas")}
+        {renderBox("Lost", lost, "Menu BI")}
+      </Box>
+    </Box>
+  )
+}
