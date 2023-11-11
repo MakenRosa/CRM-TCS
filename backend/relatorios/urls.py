3ac1aadd6794d68d51018e080633fd9a48dd7e0e
martyin.okuma <martyin.okuma@nexxera.com>
2023-11-11 20:57:53 -0300

    feat(backend): data report in pdf

--- backend/relatorios/urls.py
+++ backend/relatorios/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import criar_relatorio_excel
+from .views_excel import criar_relatorio_excel
+from .views_pdf import criar_relatorio_pdf
 
 urlpatterns = [
-    path('', criar_relatorio_excel),
+    path('excel/', criar_relatorio_excel),
+    path('pdf/', criar_relatorio_pdf),
     ]
