6c307718cf97b7945c717a49a1f1c60f3d3537d8
MakenRosa <maken.cristhian@gmail.com>
2023-11-13 16:14:56 -0300

    feat(frontend): integrate dashboard

--- frontend/src/pages/Dashboard/AnalyticsDashboard/AnalyticsDashboard .jsx
+++ frontend/src/pages/Dashboard/AnalyticsDashboard/AnalyticsDashboard .jsx
@@ -1,59 +1,58 @@
 import React, { useState, useMemo } from 'react'
 import { Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 import 'chart.js/auto'
-import { Bar, Doughnut } from 'react-chartjs-2'
-import { dadosIndicadores } from './data'
+import { Bar } from 'react-chartjs-2'
+import PropTypes from 'prop-types'
 
 const coresGrafico = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF9F40']
 
-const rotulosIndicadores = {
-  prospeccao_lead: ['Prospecção', 'Leads'],
-  proposta_prospeccao: ['Proposta', 'Prospecção'],
-  vendas_proposta: ['Vendas', 'Propostas'],
-  vendas_prospeccao: ['Vendas', 'Prospecção']
+const mapeamentoDadosBarra = {
+  prospeccao_lead: indicadores => ({
+    'Leads': indicadores.analise_quantidade_lead,
+    'Prospecções': indicadores.analise_quantidade_prospeccao
+  }),
+  proposta_prospeccao: indicadores => ({
+    'Propostas': indicadores.analise_quantidade_propostas,
+    'Prospecções': indicadores.analise_quantidade_prospeccao
+  }),
+  vendas_proposta: indicadores => ({
+    'Vendas': indicadores.analise_quantidade_venda,
+    'Propostas': indicadores.analise_quantidade_propostas
+  }),
+  vendas_prospeccao: indicadores => ({
+    'Vendas': indicadores.analise_quantidade_venda,
+    'Prospecções': indicadores.analise_quantidade_prospeccoes
+  })
 }
 
-const mapeamentoDadosTorta = {
-  prospeccao_lead: indicadores => [indicadores.total_prospeccoes, indicadores.total_leads],
-  proposta_prospeccao: indicadores => [indicadores.total_propostas, indicadores.total_prospeccoes],
-  vendas_proposta: indicadores => [indicadores.total_vendas, indicadores.total_propostas],
-  vendas_prospeccao: indicadores => [indicadores.total_vendas, indicadores.total_prospeccoes]
-}
+export const AnalyticsDashboard = ({ data }) => {
 
-export const AnalyticsDashboard = () => {
+  if (!data || Object.keys(data).length === 0 || !data.prospeccao_lead) {
+    return (
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+        <p>Dados não disponíveis.</p>
+      </Box>
+    )
+  }
   const [indiceSelecionado, setIndiceSelecionado] = useState('prospeccao_lead')
 
-  const dadosGraficoTorta = useMemo(() => {
-    const indicadores = dadosIndicadores[indiceSelecionado]
-    const rotulos = rotulosIndicadores[indiceSelecionado] || ['Rótulo 1', 'Rótulo 2']
-    const dadosIndiceSelecionado = mapeamentoDadosTorta[indiceSelecionado](indicadores)
-  
-    const dados = {
-      labels: rotulos,
-      datasets: [{
-        data: dadosIndiceSelecionado,
-        backgroundColor: coresGrafico,
-        hoverBackgroundColor: coresGrafico.map(cor => `${ cor }99`)
-      }]
-    }
-    return dados
-  }, [indiceSelecionado])
-  
+  const dadosGraficosBarra = useMemo(() => {
+    const indicadores = data[indiceSelecionado]
+    const dadosMapeados = mapeamentoDadosBarra[indiceSelecionado](indicadores)
+    const datasets = Object.keys(dadosMapeados).map((key, index) => ({
+      label: key,
+      data: Object.values(dadosMapeados[key]),
+      backgroundColor: coresGrafico[index],
+      hoverBackgroundColor: `${ coresGrafico[index]  }99`
+    }))
 
-  const dadosGraficoBarra = useMemo(() => {
-    const indicadores = dadosIndicadores[indiceSelecionado]?.analise_quantidade || {}
     return {
-      labels: Object.keys(indicadores),
-      datasets: [{
-        label: 'Quantidade por mês',
-        data: Object.values(indicadores),
-        backgroundColor: coresGrafico,
-        hoverBackgroundColor: coresGrafico.map(cor => `${ cor }99`)
-      }]
+      labels: Object.keys(data.prospeccao_lead.analise_quantidade_lead),
+      datasets
     }
   }, [indiceSelecionado])
 
-  const handleChange = event => {
+    const handleChange = event => {
     setIndiceSelecionado(event.target.value)
   }
 
@@ -76,13 +75,19 @@ export const AnalyticsDashboard = () => {
       </FormControl>
         
       <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', width: '100%', height: '100%' }}>
-        <Box sx={{ width: '40%', maxWidth: '600px', height: '300px' }}>
-          <Bar data={dadosGraficoBarra} options={{ indexAxis: 'y', maintainAspectRatio: false, scales: { y: { ticks: { autoSkip: false } } } }} />
-        </Box>
-        <Box sx={{ width: '40%', maxWidth: '600px', height: '300px' }}>
-          <Doughnut data={dadosGraficoTorta} />
-        </Box>
+        {dadosGraficosBarra.datasets.map((dataset, index) => (
+          <Box key={index} sx={{ width: '40%', maxWidth: '600px', height: '300px' }}>
+            <Bar 
+              data={{ labels: dadosGraficosBarra.labels, datasets: [dataset] }} 
+              options={{ indexAxis: 'y', maintainAspectRatio: false, scales: { y: { ticks: { autoSkip: false } } } }}
+            />
+          </Box>
+        ))}
       </Box>
     </Box>
   )
+}
+
+AnalyticsDashboard.propTypes = {
+  data: PropTypes.object
 }
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,50 +1,60 @@
-import { Box, Typography, styled } from '@mui/material'
-import { useState } from 'react'
+import { Box, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
 import { InsertChartOutlined, TroubleshootOutlined } from '@mui/icons-material'
+import { getMenuBi } from 'utils'
 import { Funnel } from './Funnel'
 import { AnalyticsDashboard } from './AnalyticsDashboard/AnalyticsDashboard '
 
-const StyledFunnelContainer = styled(Box)`
-`
-
-const StyledIndexContainer = styled(Box)`
-`
-
-const dataFunil = {
-  total_leads: 1,
-  total_prospeccoes: 1,
-  total_propostas: 3,
-  total_venda: 1
-}
-
 export const Dashboard = () => {
-  const [data, setData] = useState(dataFunil)
+  const [dataFunnel, setDataFunnel] = useState({})
+  const [data, setData] = useState({})
+
+  const user_id = sessionStorage.getItem('user_id')
+
+  useEffect(() => {
+    const fetchMenuBI = async () => {
+      const response = await getMenuBi(user_id)
+      if (response && response.data) {
+        if (response.data.funil) {
+          const { funil } = response.data
+          setDataFunnel({
+            total_leads: funil.total_leads,
+            total_prospeccoes: funil.total_prospeccoes,
+            total_propostas: funil.total_propostas,
+            total_venda: funil.total_venda
+          })
+        }
+
+        setData(response.data)
+        console.log(response.data)
+      }
+    }
+    fetchMenuBI()
+  }, [user_id])
 
   return (
     <Box display="flex" flexDirection="column" height="80vh" marginTop="20px" width="100%">
       <Box display="flex" flexDirection="row" justifyContent="space-around" width="100%">
-        <StyledFunnelContainer textAlign="center" width="40%">
+        <Box textAlign="center" width="40%">
           <Typography borderBottom="2px solid #2E1F92" color="#5038ED" marginBottom="20px" variant="h4" width="100%">
             <InsertChartOutlined />
             Funil
           </Typography> 
           <Box display="flex" justifyContent="center" marginLeft="40px" width="100%">
-            <Funnel data={data} valorVendas={220} />
+            <Funnel data={dataFunnel} valorVendas={220} />
           </Box>
-        </StyledFunnelContainer>
-        <StyledIndexContainer width="55%">
+        </Box>
+        <Box width="55%">
           <Typography borderBottom="2px solid #2E1F92" color="#5038ED" marginBottom="20px" textAlign="center" variant="h4" width="100%">
             <TroubleshootOutlined />
             Índices
           </Typography>
           <Box>
-            <AnalyticsDashboard />
+            <AnalyticsDashboard data={data} />
           </Box>
-        </StyledIndexContainer>
+        </Box>
       </Box>
-      <Box>
-        tabela
-      </Box>
+      <Box />
     </Box>
   )
 }
