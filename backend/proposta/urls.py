f9860a6300df13605d2d5fed31e8db8b59e35ad0
martyin.okuma <martyin.okuma@nexxera.com>
2023-11-13 02:37:50 -0300

    fix(backend): edit task

--- backend/proposta/urls.py
+++ backend/proposta/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PropostaView, PropostaDetails, TarefaView, VendaView, PerdidoView
+from .views import PropostaView, PropostaDetails, TarefaView, VendaView, PerdidoView, TarefaDetails
 
 urlpatterns = [
     path('', PropostaView.as_view()),
     path('<int:id>/', PropostaDetails.as_view()),
     path('tarefa/', TarefaView.as_view()),
+    path('tarefa/<int:id>/', TarefaDetails.as_view()),
     path('venda/', VendaView.as_view()),
     path('perdido/', PerdidoView.as_view()),
 ]
