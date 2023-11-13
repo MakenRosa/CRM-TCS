147eb0e53b0f5557c37283d0c078ffe309824019
makenrosa <maken.cristhian@gmail.com>
2023-11-13 01:50:31 -0300

    refactor(frontend): update basic relatorios

--- frontend/src/pages/Relatorios/SimpleFilter/SimpleFilter.jsx
+++ frontend/src/pages/Relatorios/SimpleFilter/SimpleFilter.jsx
@@ -1,64 +1,39 @@
 import { Box, Typography } from "@mui/material"
-
-import currency from 'assets/currency.svg'
-import group from 'assets/group.svg'
-import calendar from 'assets/calendar.svg'
-import percentage from 'assets/percentage.svg'
-import finish from 'assets/finish.svg'
-import lost from 'assets/lost.svg'
-import { StyledFilterBox } from "pages/Relatorios/Relatorios.styles"
-import { getFunil, getMenuBI, getPropostaProspeccao, getProspeccaoLeads, getVendasProposta, getVendasProspeccao } from "utils"
 import { toast } from "react-toastify"
-
-const API_FUNCTIONS = {
-  "Propostas P/ Prospecção": getPropostaProspeccao,
-  "Prospecções P/ Lead": getProspeccaoLeads,
-  "Vendas -> propostas": getVendasProposta,
-  "Prospecções -> vendas": getVendasProspeccao,
-  "Funil de Vendas": getFunil,
-  "Menu BI": getMenuBI
-}
+import { StyledFilterBox } from "pages/Relatorios/Relatorios.styles" // Ajuste o caminho conforme necessário
+import { PictureAsPdfOutlined, TableChartOutlined } from "@mui/icons-material"
 
 export const SimpleFilter = () => { 
   const user_id = sessionStorage.getItem("user_id")
 
-  const handleBoxClick = async title => {
-    if (API_FUNCTIONS[title]) {
-      const url = API_FUNCTIONS[title](user_id)
-      
+  const handleDownloadClick = async format => {
+    const url = `http://localhost:8000/api/relatorios/${ format }/?user_id=${ user_id }`
+    
+    try {
       const response = await fetch(url)
-  
       if (response.ok) {
         window.open(url, '_blank')
-        toast.success(`O download de ${ title } foi iniciado`)
+        toast.success(`O download do relatório em ${ format.toUpperCase() } foi iniciado.`)
       } else {
-        toast.error(`Erro ao buscar dados de ${ title }`)
+        throw new Error('Resposta da rede não foi ok.')
       }
-    } else {
-      toast.error(`Erro ao buscar dados de ${ title }`)
+    } catch (error) {
+      toast.error(`Erro ao baixar o relatório em ${ format.toUpperCase() }: ${ error }`)
     }
   }
-  
 
-  const renderBox = (alt, src, title) => (
-    <StyledFilterBox onClick={() => handleBoxClick(title)}>
-      <img alt={alt} src={src} style={{ height: "50%" }} />
-      <Typography color="#fff" variant="h6">{title}</Typography>
+  // Utilizando StyledFilterBox para renderizar os botões
+  const renderDownloadBox = (format, text, Icon) => (
+    <StyledFilterBox onClick={() => handleDownloadClick(format)} sx={{ width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', flexGrow: 1, margin: '10px 20px' }}>
+      <Icon fontSize="large" sx={{ color: '#fff', width: '50%', height: '50%' }} />
+      <Typography color="#fff" variant="h6">{text}</Typography>
     </StyledFilterBox>
   )
 
   return (
-    <Box display="flex" flexDirection="column" gap="5px" height="80%" justifyContent="center" marginTop="50px" width="80%">
-      <Box alignItems="center" display="flex" flexDirection="row" gap="5px" justifyContent="space-between">
-        {renderBox("Currency", currency, "Propostas P/ Prospecção")}
-        {renderBox("Group", group, "Prospecções P/ Lead")}
-        {renderBox("Calendar", calendar, "Vendas -> propostas")}
-      </Box>
-      <Box alignItems="center" display="flex" flexDirection="row" gap="5px" justifyContent="space-between">
-        {renderBox("Percentage", percentage, "Prospecções -> vendas")}
-        {renderBox("Finish", finish, "Funil de Vendas")}
-        {renderBox("Lost", lost, "Menu BI")}
-      </Box>
+    <Box display="flex" flexDirection="row" justifyContent="center" marginTop="50px" width="100%">
+      {renderDownloadBox('pdf', 'Baixar Relatório Geral (PDF)', PictureAsPdfOutlined)}
+      {renderDownloadBox('excel', 'Baixar Relatório Geral (Excel)', TableChartOutlined)}
     </Box>
   )
 }
